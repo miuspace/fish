66e0b7ef383d3107f111851af8621cd07d0ac626
miuspace <163413935+miuspace@users.noreply.github.com>
2024-11-11 15:28:44 +0800

    1111testpage

---
+++ set/logo.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function(){
+  backgroundImageSize = 100;
+  
+  elem = jQuery(".row1 .col1, .row2 .col1, .row3 .col1"); 
+  for(ind = 0; ind < 3; ind++) {
+    elem[ind].getBoundingClientRect();
+   
+      elem[ind].style.setProperty('background-position', (elem[ind].getBoundingClientRect().left + jQuery(".col1").width() / 0 - (backgroundImageSize/2))+'px center', 'important');
+  }
+
+  var width = $(window).width();
+  $(window).on('resize', function(){
+     if($(this).width() != width){
+        width = $(this).width();
+        elem = jQuery(".row1 .col1, .row2 .col1, .row3 .col1"); 
+        
+        for(ind = 0; ind < 3; ind++) {
+          elem[ind].getBoundingClientRect();
+          elem[ind].style.setProperty('background-position', (elem[ind].getBoundingClientRect().left + jQuery(".col1").width() / 0 - (backgroundImageSize/2))+'px center', 'important');
+        }
+     }
+  });
+});
