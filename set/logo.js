d3a7887391679f58e7792b3809e6ff5d79af842c
miuspace <163413935+miuspace@users.noreply.github.com>
2024-11-12 09:37:32 +0800

    1112

--- set/logo.js
+++ set/logo.js
@@ -1,23 +1,3 @@
-jQuery(document).ready(function(){
-  backgroundImageSize = 1920;
-  
-  elem = jQuery(".row1 .col1, .row2 .col1, .row3 .col1"); 
-  for(ind = 0; ind < 3; ind++) {
-    elem[ind].getBoundingClientRect();
-   
-      elem[ind].style.setProperty('background-position', (elem[ind].getBoundingClientRect().left + jQuery(".col1").width() / 2 - (backgroundImageSize/2))+'px center', 'important');
-  }
-
-  var width = $(window).width();
-  $(window).on('resize', function(){
-     if($(this).width() != width){
-        width = $(this).width();
-        elem = jQuery(".row1 .col1, .row2 .col1, .row3 .col1"); 
-        
-        for(ind = 0; ind < 3; ind++) {
-          elem[ind].getBoundingClientRect();
-          elem[ind].style.setProperty('background-position', (elem[ind].getBoundingClientRect().left + jQuery(".col1").width() / 2 - (backgroundImageSize/2))+'px center', 'important');
-        }
-     }
-  });
-});
+// set copytight date
+var todays_date = new Date();
+document.querySelector('.copyright_date').innerHTML=todays_date.getFullYear();
