e13e91aabb3b38d8bef2c75b839bca69cdf0a566
miuspace <163413935+miuspace@users.noreply.github.com>
2025-09-18 17:20:23 +0800

    5

--- index/main.js
+++ index/main.js
@@ -73,3 +73,39 @@ function initBackgroundSwitcher() {
 document.addEventListener('DOMContentLoaded', () => {
   initBackgroundSwitcher();
 });
+
+
+function initParallaxEffect() {
+  const line1s = document.querySelectorAll('.deco-line-1');
+  const line2s = document.querySelectorAll('.deco-line-2');
+
+  let latestScrollY = 0;
+  let ticking = false;
+
+  function updateParallax() {
+    const offset1 = latestScrollY * 0.08;//0.3
+    const offset2 = latestScrollY * 0.2;//0.5
+
+    line1s.forEach(el => {
+      el.style.transform = `translateY(${offset1}px)`;
+    });
+    line2s.forEach(el => {
+      el.style.transform = `translateY(${offset2}px)`;
+    });
+
+    ticking = false;
+  }
+
+  window.addEventListener('scroll', () => {
+    latestScrollY = window.scrollY;
+
+    if (!ticking) {
+      window.requestAnimationFrame(updateParallax);
+      ticking = true;
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  initParallaxEffect();
+});
